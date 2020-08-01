dee390498f534d4abe5dcb5d4b3317d68849c902
OksanaGood <oksana.shnurkina.76@mail.ru>
2020-08-01 11:52:29 -0400

    update Happy B20

--- src/HappyB20.java
+++ src/HappyB20.java
@@ -1,6 +1,13 @@
+import com.sun.xml.internal.ws.api.model.wsdl.WSDLOutput;
+
 public class HappyB20 {
 
     public static void main(String[] args) {
         System.out.println("B20 is always happy");
+
+        System.out.println("I love Java");
     }
+
 }
+
+
